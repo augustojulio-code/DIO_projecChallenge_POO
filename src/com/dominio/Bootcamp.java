4d390dd5ebbc3efb1e525da5d66e78257b0713ab
Julio <julio.mech@hotmail.com>
2023-03-10 09:19:25 -0300

    building domain classes

--- src/com/dominio/Bootcamp.java
+++ src/com/dominio/Bootcamp.java
@@ -51,6 +51,60 @@ public class Bootcamp {
 	public void setConteudos(Set<Conteudo> conteudos) {
 		this.conteudos = conteudos;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((conteudos == null) ? 0 : conteudos.hashCode());
+		result = prime * result + ((dataInicio == null) ? 0 : dataInicio.hashCode());
+		result = prime * result + ((datafinal == null) ? 0 : datafinal.hashCode());
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((devs == null) ? 0 : devs.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bootcamp other = (Bootcamp) obj;
+		if (conteudos == null) {
+			if (other.conteudos != null)
+				return false;
+		} else if (!conteudos.equals(other.conteudos))
+			return false;
+		if (dataInicio == null) {
+			if (other.dataInicio != null)
+				return false;
+		} else if (!dataInicio.equals(other.dataInicio))
+			return false;
+		if (datafinal == null) {
+			if (other.datafinal != null)
+				return false;
+		} else if (!datafinal.equals(other.datafinal))
+			return false;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (devs == null) {
+			if (other.devs != null)
+				return false;
+		} else if (!devs.equals(other.devs))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
 	
 	
 
